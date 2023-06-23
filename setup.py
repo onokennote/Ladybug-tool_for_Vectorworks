b7af20cc7a3bb57aa9e9e9b686d730273912deac
onokennote <113188583+onokennote@users.noreply.github.com>
2023-06-24 07:51:49 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 setuptools.setup(
 	name="ladybug_vectorworks",
-	version="0.2.1",
+	use_scm_version=True,
+	setup_requires=['setuptools_scm'],
 	author="onokennote",
 	author_email="info@onoken-web.com",
 	description="A library for communicating between Ladybug Tools core libraries and Vectorwroks CAD.",
